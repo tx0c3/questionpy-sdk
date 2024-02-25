16ea5c981bfdfbe6114c9dea0c52f7697ede9e62
Mirko Dietrich <dietrich@math.tu-berlin.de>
2024-02-25 13:54:27 +0100

    test: put test_run tests into isolated FS
    Ensures test_run passes regardless of cwd.

--- tests/cli/test_run.py
+++ tests/cli/test_run.py
@@ -2,6 +2,8 @@
 #  The QuestionPy SDK is free software released under terms of the MIT license. See LICENSE.md.
 #  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
 
+import os
+from pathlib import Path
 from click.testing import CliRunner
 
 from questionpy_sdk.commands.run import run
@@ -17,20 +19,26 @@ def test_run_no_arguments() -> None:
 
 def test_run_with_not_existing_package() -> None:
     runner = CliRunner()
-    result = runner.invoke(run, ["package.qpy"])
-    assert result.exit_code != 0
-    assert "'package.qpy' doesn't look like a QPy package zip file, directory or module" in result.stdout
+    with runner.isolated_filesystem():
+        result = runner.invoke(run, ["package.qpy"])
+        assert result.exit_code != 0
+        assert "'package.qpy' doesn't look like a QPy package zip file, directory or module" in result.stdout
 
 
 def test_run_non_zip_file() -> None:
     runner = CliRunner()
-    result = runner.invoke(run, ["README.md"])
-    assert result.exit_code != 0
-    assert "'README.md' doesn't look like a QPy package zip file, directory or module" in result.stdout
+    with runner.isolated_filesystem() as fs:
+        with open(Path(fs) / "README.md", "w", encoding="utf-8") as f:
+            f.write("Foo bar")
+        result = runner.invoke(run, ["README.md"])
+        assert result.exit_code != 0
+        assert "'README.md' doesn't look like a QPy package zip file, directory or module" in result.stdout
 
 
 def test_run_dir_without_config() -> None:
     runner = CliRunner()
-    result = runner.invoke(run, ["tests"])
-    assert result.exit_code != 0
-    assert f"The config 'tests/{PACKAGE_CONFIG_FILENAME}' does not exist" in result.stdout
+    with runner.isolated_filesystem() as fs:
+        os.mkdir(Path(fs) / "tests")
+        result = runner.invoke(run, ["tests"])
+        assert result.exit_code != 0
+        assert f"The config 'tests/{PACKAGE_CONFIG_FILENAME}' does not exist" in result.stdout
